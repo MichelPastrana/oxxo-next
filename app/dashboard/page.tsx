bfe916f48ab3fa03080b9683e9cca56c5beae577
MichelPastrana <michelpach13@hotmail.com>
2024-10-25 22:15:42 -0600

    feat: middleware

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 export default function DashboardPage() {
     return (
         <>
-            <div className="h-full w-[50vw] bg-red-300">
+            <div className="h-full w-4/12 bg-red-300">
                 <p>Hola soy section 1</p>
             </div>
-            <div className="h-full w-[40vw] bg-red-300">
+            <div className="h-full w-5/12 bg-red-300">
                 <p>Hola soy section 2</p>
             </div>
         </>
