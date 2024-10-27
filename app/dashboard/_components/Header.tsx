cd336251e0637a6ea3351b0127cd0f1f9e27c816
MichelPastrana <michelpach13@hotmail.com>
2024-10-26 18:06:18 -0600

    feat: Cards for employees locations

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header(){
     return (
-        <div className="w-screen h-[10vh] bg-gray-300 flex flex-row items-center px-10">
+        <div className="w-screen h-[10vh] bg-gray-200 flex flex-row items-center px-10">
             <Image src="oxxo_logo.svg" alt="Oxxo logo" width={100} height={0} draggable={false}/>
         </div>
     )
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,18 +3,18 @@ import Sidebar from "./_components/_sidebar/Sidebar";
 
 export default function LayoutDashboard({
     children,
-    count
+    locations,
 }: Readonly<{
     children: React.ReactNode;
-    count: React.ReactNode;
+    locations: React.ReactNode;
 }>){
     return (
-    <div className="bg-gray-700">
+    <div className="bg-gray-500">
         <Header/>
         <div className="flex flex-row items-center">
             <Sidebar/>
             {children}
-            {count}
+            {locations}
         </div>
     </div>
     )
