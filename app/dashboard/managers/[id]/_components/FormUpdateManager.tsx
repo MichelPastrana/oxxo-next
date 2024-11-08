e3c60199ef773eb2b26c784be2c882eaa5a97357
MichelPastrana <michelpach13@hotmail.com>
2024-11-08 11:31:20 -0600

    modal para actualizar manager completado

--- app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
+++ app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
@@ -14,27 +14,35 @@ export default async function FormUpdateManager({manager} : {manager: Manager}) 
     })
     const stores = await responseStores.json()
     return(
-        <form action={updateManagerWithId} className="bg-gray-300 rounded-md">
+        <form action={updateManagerWithId} className="bg-gray-300 rounded-md flex flex-col flex-grow-0 gap-2">
             <h1>Actualizar Manager</h1>
             <Input
+            label="Nombre completo"
+            isRequired
             defaultValue={manager.managerFullName}
             placeholder="Marco Aurelio"
             name="managerFullName"
             />
             <Input
+            label="Email"
+            isRequired
             defaultValue={manager.managerEmail}
             placeholder="manger@oxxo.com"
             name="managerEmail"
             />
             <Input
+            label="Salario"
+            isRequired
             defaultValue={String(manager.managerSalary)}
-            placeholder="manger@oxxo.com"
-            name="12000"
+            placeholder="12000"
+            name="managerSalary"
             />
             <Input
+            label="Numero de telefono"
+            isRequired
             defaultValue={manager.managerPhoneNumber}
-            placeholder="manger@oxxo.com"
-            name="4421333617"
+            placeholder="4421333617"
+            name="managerPhoneNumber"
             />
             <SelectStore stores={stores} defaultStore={manager?.location?.locationId}/>
             <Button color="primary" type="submit">
